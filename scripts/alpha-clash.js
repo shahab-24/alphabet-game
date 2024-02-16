320f6ed9c0d04d1b470dd3796450bee26e3acbeb
Shahab Uddin <shahab.82946@gmail.com>
2024-02-16 11:16:17 +0600

    completed full project

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -5,8 +5,93 @@
 //   const playgroundSection = document.getElementById('play-ground');
 //   playgroundSection.classList.remove('hidden');
 // }
+function handleKeyboardKeyupEvent(event){
+  const pressedKey = event.key;
+  if(pressedKey === 'Escape'){
+    gameOver()
+  }
+
+
+  const showedKey = document.getElementById('current-alphabet');
+  const getKey = showedKey.innerText
+  const expectedKey = getKey.toLowerCase();
+
+  if(pressedKey === expectedKey){
+    console.log('you got a point');
+    
+    // const currentScoreElement = document.getElementById('current-score');
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+
+    // const newScore = currentScore + 1;
+    // currentScoreElement.innerText = newScore;
+
+    const currentScore = getElementValueById('current-score');
+    const updatedScore = currentScore + 1;
+
+    setElementValueById('current-score', updatedScore);
+
+    removeBackgroundById(expectedKey);
+    continueGame();
+  }
+else{
+  console.log('you have lost a life');
+
+  const currentLife = getElementValueById('current-life');
+  const updatedLife = currentLife - 1;
+  setElementValueById('current-life', updatedLife);
+
+  if(updatedLife === 0){
+    gameOver()
+    
+  }
+
+  // ----------------------------------
+  // const currentLifeElement = document.getElementById('current-life');
+  // const currentLifeText = currentLifeElement.innerText;
+  // const currentLife = parseInt(currentLifeText);
+
+  // const newLife = currentLife - 1;
+  // currentLifeElement.innerText = newLife;
+}
+}
+
+
+document.addEventListener('keyup', handleKeyboardKeyupEvent);
+
+function continueGame(){
+  const alphabet = getArandomAlphabet();
+
+  console.log('your current alphabet is', alphabet);
+
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+  
+  setBackgroundById(alphabet);
+} 
+
+
 
 function play(){
   hideElementById('home-screen');
+  hideElementById('final-score');
   showElementById('play-ground');
+
+  setElementValueById('current-life', 5)
+  setElementValueById('current-score', 0)
+ 
+
+  continueGame();
+}
+
+function gameOver(){
+  hideElementById('play-ground');
+  showElementById('final-score');
+
+  const lastScore = getElementValueById('current-score');
+  setElementValueById('last-score', lastScore);
+
+  const currentAlphabet = getElementTextById('current-alphabet');
+  
+  removeBackgroundById(currentAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,46 @@ function showElementById(elementId){
   const element = document.getElementById(elementId);
   element.classList.remove('hidden');
 }
+
+function setBackgroundById(elementId){
+  const alphabetById = document.getElementById(elementId);
+  alphabetById.classList.add('bg-orange-400');
+}
+function removeBackgroundById(elementId){
+  const element = document.getElementById(elementId);
+  element.classList.remove('bg-orange-400');
+}
+
+function getElementValueById(elementId){
+  const element = document.getElementById(elementId);
+  const elementText = element.innerText;
+  const value = parseInt(elementText);
+  return value;
+
+}
+
+function setElementValueById(elementId, value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+
+}
+
+function getElementTextById(elementId){
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
+}
+
+
+
+
+function getArandomAlphabet(){
+  const randomAlphabets = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabets = randomAlphabets.split('');
+  
+
+  const getRandomNumber = Math.random()*25;
+  const index = Math.round(getRandomNumber);
+  const alphabet = alphabets[index];
+  return alphabet;
+}
